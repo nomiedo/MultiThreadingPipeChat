6c76c497c090afbc5da4db0a7fef94b80b46e057
aleksand zareckij <zareckij.aleksandr@gmail.com>
2017-10-16 21:08:34 +0300

    Initializing

---
+++ Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new NamedPipeClientStream("TestPipe");
+            client.Connect();
+            StreamReader reader = new StreamReader(client);
+            StreamWriter writer = new StreamWriter(client);
+
+            Guid name = Guid.NewGuid();
+            Random rnd = new Random();
+
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    string message = name + "message" + rnd.Next();
+                    writer.WriteLine(message);
+                    writer.Flush();
+                }
+            });
+
+            while (true)
+            {
+                var line = reader.ReadLine();
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        bool running;
+        Thread runningThread;
+        Thread messageThread;
+        EventWaitHandle terminateHandle = new EventWaitHandle(false, EventResetMode.AutoReset);
+        List<string> messages = new List<string>();
+ 
+        static void Main(string[] args)
+        {
+            Program server = new Program();
+            server.Run();
+        }
+
+        void ServerLoop()
+        {
+            while (running)
+            {
+                ProcessNextClient();
+            }
+
+            terminateHandle.Set();
+        }
+
+        public void Run()
+        {
+            running = true;
+
+            runningThread = new Thread(ServerLoop);
+            runningThread.Start();
+
+            Console.WriteLine("Server is started");
+        }
+
+        public void Stop()
+        {
+            running = false;
+            terminateHandle.WaitOne();
+        }
+
+        public void ProcessNextClient()
+        {
+            NamedPipeServerStream pipeStream = new NamedPipeServerStream("TestPipe", PipeDirection.InOut, 254);
+            StreamReader reader = new StreamReader(pipeStream);
+            StreamWriter writer = new StreamWriter(pipeStream);
+
+            pipeStream.WaitForConnection();
+
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    var line = reader.ReadLine();
+                    messages.Add(line);
+                    Console.WriteLine(line);
+
+                        writer.WriteLine(line);
+                        writer.Flush();
+
+                    //foreach (var message in messages)
+                    //{
+                    //    writer.WriteLine(message);
+                    //    writer.Flush();
+                    //}
+                }
+            });
+
+            
+        }
+    }
+}
